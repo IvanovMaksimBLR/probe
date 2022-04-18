235e53460588a625ee3b31d137b3478eee357d6f
Maksim <maskimangerman@gmail.com>
2022-04-18 16:59:53 +0300

    исправлены ошибки. но требуются доработки)

--- family.py
+++ family.py
@@ -1,7 +1,12 @@
-# задачи: создать класс семья(Jon, Kat), создать класс домашний питомец (Cihua)
-# откладывать зп в конце
-# месяца на покупку квартиры( цикл дней должен закнчится когда будет собранна нужнная сумма)
-
+#цель: отработка навыков работы с классами
+#семья собирает на новую квартру
+#немного топорный вариант)
+#требуются доработки в:
+#указать кто на какую работу ходит
+#кто каким спортом занимается
+# доработать выгул питомца
+#
+#
 
 from random import randint
 
@@ -13,34 +18,36 @@ class Family:
     def __init__(self, name):
         self.name = name
         self.fullness = 50
-        self.train = 0
+        self.train = 50
         self.house = None
 
     def __str__(self):
-        return 'Я - {}, сыт на  {}, физическая форма на '.format(
+        return 'Я - {}, сыт на  {}, физическая форма на {} '.format(
             self.name, self.fullness, self.train)
 
     def shopping(self):
-        if self.house.money >= 50:
+        if self.house.food <= 20:
             cprint('{} сходил в магазин за едой'.format(self.name), color='magenta')
-            self.house.money -= 50
-            self.house.food += 50
-        else:
-            cprint('{} деньги кончились!'.format(self.name), color='red')
+            self.house.money -= 100
+            self.house.food += 100
+            # self.fullness -= 2
+
 
     # приготовили еды из продуктов
     def cooking(self):
-        if self.house.dish <= 10:
+        if self.house.dish <= 50:
             cprint('{} приготовил вкуснейшей еды'.format(self.name), color='yellow')
-            self.house.dish += 50
-            self.house.food -= 50
+            self.house.dish += 100
+            self.house.food -= 100
+            # self.fullness -= 5
 
     # поели готовой еды
     def eat(self):
-        if self.house.dish >= 10:
+        if self.house.dish >= 20:
             cprint('{} поел'.format(self.name), color='yellow')
-            self.fullness += 10
+            self.fullness += 30
             self.house.dish -= 10
+
         else:
             cprint('{} нет еды'.format(self.name), color='red')
 
@@ -51,43 +58,48 @@ class Family:
         self.train += 50
         self.fullness -= 10
 
+
     # каждый ходит на конкретно свою работу(разработчик, айчар)
+
     def work(self):
         cprint('{} сходил на работу'.format(self.name), color='blue')
         self.house.money += 250
         self.fullness -= 10
+        self.house.dish -= 5
+        self.train -= 10
 
     # уборка квартиры
     def cleaning(self):
         cprint('{} убрал в квартире'.format(self.name), color='blue')
-        self.house.clean += 10
+        self.house.clean += 100
+        # self.fullness -= 5
 
     # выгуливать питомца. (доработать логику)
     def walking(self):
         cprint('{} выгулял чихуяшку'.format(self.name), color='green')
-        self.fullness -= 3
+        self.fullness -= 10
 
     def go_to_the_house(self, house):
         self.house = house
         self.fullness -= 10
         cprint('{} Вьехал в дом'.format(self.name), color='cyan')
 
-    # переработать логику действия.
+    # переработать логику действий.
     def act(self):
-        if self.fullness <= 0:
-            cprint('{} охренел...'.format(self.name), color='red')
+        if self.train <= 10:
+            cprint('{} пора заняться собой...'.format(self.name), color='red')
             return
-        dice = randint(1, 6)
-        if self.fullness < 20:
+
+        if self.fullness < 10:
             self.eat()
         elif self.house.food < 10:
             self.shopping()
-#        elif self.house.money < 50:
-#           self.work()
-        elif dice == 1:
-            self.work()
-        elif dice == 2:
-            self.eat()
+        elif self.train <= 100:
+            self.training()
+        elif self.house.dish <= 10:
+            self.cooking()
+        elif self.house.clean <= 0:
+            self.cleaning()
 
 
 
@@ -113,16 +125,24 @@ residents = [
 rent_house = House()
 for citisen in residents:
     citisen.go_to_the_house(house=rent_house)
-# в этом блоке изменить цель. while количество денег на новую квартиру. прервать цикл. указать сколько понадобилось
-# дней.
+
 #решить проблему "накопления" дней + накопления денег
-cash = House.self.money()
-while cash < 41000:
-    print('================ день {} ==================')
+# счетчик циклов?
+day = 1
+while rent_house.money <= 1500000:
+
+    print('==============начался новый день №', day, '===================')
+    day += 1
     for citisen in residents:
         citisen.act()
         citisen.work()
-    print('------------ в конце дня -------------')
+        citisen.eat()
+        citisen.walking()  #придумать как выгуливать по очереди или когда нужно
+    print('--- в конце дня ---')
     for citisen in residents:
         print(citisen)
     print(rent_house)
+
+years = round(day / 365, 2)
+cprint('Собрали на квартиру! понадобилось {} лет'.format(years), color='cyan')
+cprint('ура! своя квартира!!!', color='cyan')
