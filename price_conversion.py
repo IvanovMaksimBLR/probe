bce1b3eea06c52911141acbec0865e5cbd56e374
Maksim <maskimangerman@gmail.com>
2022-04-23 23:06:11 +0300

    ошибка. ищу способ решить проблему

--- price_conversion.py
+++ price_conversion.py
@@ -5,7 +5,7 @@
 #
 
 d_1 = 2    #курс у.е на момент покупки товара
-d_2 = 3    #курс у.е. на данный маомент. ( далее нужно будет найти способ запарсить его)
+d_2 = 2    #курс у.е. на данный маомент. ( далее нужно будет найти способ запарсить его)
 nds = 0.10 #ставка ндс 10%
 prfibility = 0.15 # % заложенной рентабельности
 # список товаров по закупочной цене в у.е
@@ -24,16 +24,24 @@ for goods in goods.items():
     goods_2.update({goods[0]:
                         {'purchase': goods[1] * d_1,
                          'nds': goods[1]*d_1 * nds,
-                         'profibility': goods[1]*d_1 * prfibility}
+                         'profibility': goods[1]*d_1 * prfibility,
+                         'total price': goods[1]*d_1 * (10 * prfibility) * (10 * nds)}
                     })
     goods_3.update({goods[0]:
                         {'purchase': goods[1] * d_2,
                          'nds': goods[1]*d_2 * nds,
-                         'profibility': goods[1]*d_2 * prfibility}
+                         'profibility': goods[1]*d_2 * prfibility,
+                         'total price': goods[1]*d_2 * (10 * prfibility) * (10 * nds)}
                     })
 print(goods_2)
 print(goods_3)
 
+x = goods_2['nuts']['total price'] - goods_3['nuts']['profibility'] - goods_2['nuts']['purchase']- goods_2['nuts']['nds']
+#решить как подступиться без имени
+print(x)
 
-
-
+if x >= 0:
+    for goods_2 in goods_2.items():
+        print('товар-', goods_2[0], 'имеет цену', goods_2['price'])
+    else:
+        print('товар-', goods_3[0], 'имеет цену', goods_3['price'])
